8e5dc7ab6d8e874eb1a4a63fbe592eeefdff4bb3
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-05 20:54:09 +0300

    Test framework is prepared

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -21,7 +21,7 @@ public class AmazonPage {
     @FindBy(xpath = "(//span[.='Add to Cart']//span)[1]")
     public WebElement addToCart;
 
-    @FindBy(css = "span.a-dropdown-label")
+    @FindBy(xpath = "//span[.='Featured']")
     public WebElement sort;
 
     @FindBy(css = "a#s-result-sort-select_1")
