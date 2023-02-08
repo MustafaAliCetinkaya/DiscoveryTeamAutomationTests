3ca6f9deb2c8859db7a2ade49eab51cdffee8084
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-08 16:08:36 +0300

    Test framework is updated

--- src/test/java/stepDefinitions/cydeoStepDefinitions/AmazonSearchStepDefinitions.java
+++ src/test/java/stepDefinitions/cydeoStepDefinitions/AmazonSearchStepDefinitions.java
@@ -1,5 +1,6 @@
 package stepDefinitions.cydeoStepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,7 +10,8 @@ import utilities.Driver;
 
 public class AmazonSearchStepDefinitions {
 
-    AmazonPage amazonPageObject=new AmazonPage();
+    AmazonPage amazonPageObject = new AmazonPage();
+
     @Given("user navigate to the amazon homepage")
     public void user_navigate_to_the_amazon_homepage() {
         Driver.getDriver().get(ConfigurationReader.getProperty("amazon"));
@@ -20,25 +22,25 @@ public class AmazonSearchStepDefinitions {
         amazonPageObject.searchItem("game chair");
     }
 
-    @Then("user sorts the items by price from low to high")
+    @And("user sorts the items by price from low to high")
     public void user_sorts_the_items_by_price_from_low_to_high() {
         amazonPageObject.sortItemsByPrice();
 
     }
 
-    @Then("user adds the selected items to the cart one by one")
+    @And("user adds the selected items to the cart one by one")
     public void user_adds_the_selected_items_to_the_cart_one_by_one() {
         amazonPageObject.addItem(amazonPageObject.gameChair);
     }
 
-    @Then("user goes to the cart")
+    @And("user goes to the cart")
     public void user_goes_to_the_cart() {
         amazonPageObject.goCart();
     }
 
     @Then("user goes to the checkout page")
     public void user_goes_to_the_checkout_page() {
-amazonPageObject.goCheckoutPage();
+        amazonPageObject.goCheckoutPage();
     }
 
 }
--- src/test/resources/features/cydeoFeatures/amazonSearch.feature
+++ src/test/resources/features/cydeoFeatures/amazonSearch.feature
@@ -3,7 +3,7 @@ Feature: Amazon Search
   Scenario: TC01_User should be able to search item at the amazon website
     Given user navigate to the amazon homepage
     When user writes different items to the search box one by one
-    Then user sorts the items by price from low to high
+    And user sorts the items by price from low to high
     And user adds the selected items to the cart one by one
     And user goes to the cart
-    And user goes to the checkout page
+    Then user goes to the checkout page
