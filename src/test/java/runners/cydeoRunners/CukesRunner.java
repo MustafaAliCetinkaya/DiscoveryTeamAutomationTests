ca11a13a442706708af3f7c4d162a397ffb7dd02
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-09 15:47:37 +0300

    All step definitions and all feature files are created. Ready for testing.

---
+++ src/test/java/runners/cydeoRunners/CukesRunner.java
@@ -0,0 +1,24 @@
+package runners.cydeoRunners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
+    },
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        dryRun = false,
+        tags = "@Regression and @login",
+        publish = false
+
+)
+public class CukesRunner {
+}
+
---
+++ src/test/java/runners/cydeoRunners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package runners.cydeoRunners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "stepDefinitions",
+        features = "@target/rerun.txt"
+)
+public class FailedTestRunner {
+}
